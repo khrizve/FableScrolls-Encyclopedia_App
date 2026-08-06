78a8c761a4b36c8065206ad2b5cfb6cd1189f8e6
Sorcerer <142784752+khrizve@users.noreply.github.com>
2026-08-06 18:31:48 +0600

    Delete runewiki_src directory

--- runewiki_src/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.fablescrolls;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {PDFEntity.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract PDFDao pdfDao();
-}
